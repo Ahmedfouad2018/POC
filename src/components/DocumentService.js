565337d7015920eb733bcd4cbb784d8e0dfca561
ahmed shouab <Ahmed.Shouab@integrant.com>
2020-06-20 13:42:28 +0200

    first commit

---
+++ src/components/DocumentService.js
@@ -0,0 +1,19 @@
+import axios from 'axios'
+const baseUrl = "http://localhost:3001/document/"
+
+class DocumentService {
+
+    static postApi(url, data) {
+        return axios.post(baseUrl + url, data);
+    }
+
+    static getApi(url) {
+        return axios.get(baseUrl + url);
+    }
+
+    static deleteApi(url) {
+        return axios.delete(baseUrl + url);
+    }
+}
+
+export default DocumentService
--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+// import App from './App';
+import AddDocument from './components/AddDocument';
+import ListDocument from './components/ListDocument';
+import EditDocument from './components/EditDocument';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router>
+    <div>
+      <Route exact path='/' component={ListDocument} />
+      <Route path='/add-document' component={AddDocument} />
+      <Route path='/index' component={ListDocument} />
+      <Route path='/edit-document/:id' component={EditDocument} />
+    </div>
+  </Router>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
